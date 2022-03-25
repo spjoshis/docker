0b04fb4dc08bf95b7ded1fef63e9e087957a4197
Gopal Joshi <gopal.josh73@gmail.com>
2022-03-25 16:03:21 +0400

    Setup django app

--- docker-django-postgresql/src/user/settings.py
+++ docker-django-postgresql/src/user/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^k_j9!4y&bxmxuf*@pqg@y&z_3%a8vf5xx4oj)sujjr865ve4+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
