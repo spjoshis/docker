be81841659dab69a36f99c41e88dce46eddd5438
Gopal Joshi <gopal.josh73@gmail.com>
2022-03-31 11:33:37 +0400

    Removed security key from settings

--- docker-django-postgresql/src/user/settings.py
+++ docker-django-postgresql/src/user/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^k_j9!4y&bxmxuf*@pqg@y&z_3%a8vf5xx4oj)sujjr865ve4+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
 
 
 # Application definition
