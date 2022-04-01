67cab8af84ede57b533e94534d3baebe5f4d62f2
Gopal Joshi <gopal.josh73@gmail.com>
2022-04-01 07:22:26 +0400

    Create product page

--- docker-django-postgresql/src/pages/views.py
+++ docker-django-postgresql/src/pages/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 
 def home_view(request, *args, **kwargs   ):
     return render(request, 'home.html', {})
-    # return HttpResponse("<h1>Hi {0}</h1>".format(request.user))
 
-def category_view(request, *args, **kwargs):
-    return render(request, 'category.html', {})
+
+def cms_view(request, *args, **kwargs):
+    contex = {
+        'title': 'About',
+        'text': [
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has beenthe industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has beenthe industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has beenthe industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+        ]
+    }
+    return render(request, 'cms.html', contex)
--- docker-django-postgresql/src/products/views.py
+++ docker-django-postgresql/src/products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def category_view(request, *args, **kwargs):
+
+    # Get products
+    products = Product.objects.all()
+    params = {
+        'name': 'Products',
+        'products': products
+    }
+    return render(request, 'products/category.html', params)
